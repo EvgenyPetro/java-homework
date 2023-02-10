c07024ac06ddb9d30eec79131c766dbbd3a0f8c9
Evgeny Petrov <petroves88@mail.ru>
2023-02-10 16:45:27 +0300

    Solution1

--- Homework1/Main.java
+++ Homework1/Main.java
@@ -1,7 +1,30 @@
+//+Написать программу вычисления n-ого треугольного числа.
 package Homework1;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static void main(String[] args) {
+        int n = inputDigitN();
+        int number = findNumber(n);
+        printNumber(number);
+
+    }
+
+
+    public static int inputDigitN() {
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        String stringValue = scanner.next();
+        return Integer.parseInt(stringValue);
+    }
+
+    public static int findNumber(int n) {
+        return ((n + 1) * n) / 2;
+    }
+
+    public static void printNumber(int number) {
+        System.out.println("n-е треугольное число: " + number);
     }
 }
